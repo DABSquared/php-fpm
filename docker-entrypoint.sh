7bd7b02b81143ba669deec4218c764f2275cecd5
Daniel Brooks <dbrooks@dabsquared.com>
2017-08-03 23:11:31 -0700

    Fixing command issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -44,19 +44,19 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ];  then
        composer install --optimize-autoloader --no-interaction --no-dev || (echo >&2 "Composer Install Prod Failed" && exit 1)
     fi
 
-    php bin/console -d newrelic.appname="$symfony_app_name" --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
-    php bin/console -d newrelic.appname="$symfony_app_name" --env="$ENVIRONMENT" assets:install web || (echo >&2 "Assetic Install Failed" && exit 1)
+    php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
+    php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" assets:install web || (echo >&2 "Assetic Install Failed" && exit 1)
 
     if [ "$ISDEV" == "true" ]; then
-        php bin/console -d newrelic.appname="$symfony_app_name" --env="$ENVIRONMENT" assetic:dump --no-interaction || (echo >&2 "Assetic Dump Dev Failed" && exit 1)
+        php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction || (echo >&2 "Assetic Dump Dev Failed" && exit 1)
     else
-        php bin/console -d newrelic.appname="$symfony_app_name" --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug || (echo >&2 "Assetic Dump Prod Failed" && exit 1)
+        php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug || (echo >&2 "Assetic Dump Prod Failed" && exit 1)
     fi
 
     if [ "$ISDEV" == "true" ]; then
-        php bin/console -d newrelic.appname="$symfony_app_name" --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
+        php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
     else
-        php bin/console -d newrelic.appname="$symfony_app_name" --env="$ENVIRONMENT" cache:warmup --no-debug || (echo >&2 "Cache Warmup Prod Failed" && exit 1)
+        php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" cache:warmup --no-debug || (echo >&2 "Cache Warmup Prod Failed" && exit 1)
     fi
 
     if [ "$ISDEV" == "true" ]; then
@@ -64,7 +64,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ];  then
             numdirs=$(ls -l "$DB_DIR" | grep -v ^d | wc -l | xargs)
             echo "Number of db directories is $numdirs"
             if  [ $numdirs -le 2 ]; then
-                php bin/console -d newrelic.appname="$symfony_app_name" --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || (echo >&2 "Doctrine Fixtures Failed" && exit 1)
+                php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || (echo >&2 "Doctrine Fixtures Failed" && exit 1)
             fi
     fi
 fi
